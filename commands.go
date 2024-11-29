ed9ceb5483c8cad9d9f548a4907e30fcebe16ffe
charbonnev <charles.bonnevialle@gmail.com>
2024-11-29 09:50:50 -0300

    Api dependent structs and consts to its own file

--- commands.go
+++ commands.go
@@ -8,18 +8,6 @@ import (
 	"os"
 )
 
-type PokeLocationBody struct {
-	Count    int     `json:"count"`
-	Next     string  `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
-}
-
-var baseURL = "https://pokeapi.co/api/v2/"
-
 func ExitCommand() error {
 	os.Exit(0)
 	return nil
---
+++ pokeApi.go
@@ -0,0 +1,13 @@
+package main
+
+type PokeLocationBody struct {
+	Count    int     `json:"count"`
+	Next     string  `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+var baseURL = "https://pokeapi.co/api/v2/"
